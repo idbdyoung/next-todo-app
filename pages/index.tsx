93bed04f9fe804b69eb9f962309d26ffbb64602b
idbd <idbd.young@gmail.com>
2021-03-01 16:59:02 +0900

    feat: Add style by client-side-rendering(styled-component)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100px;
+  padding: 10px;
+  background: yellow;
+  & div {
+    width: 100%;
+    height: 50px;
+    background: red;
+  }
+`;
 
 const index: NextPage = () => {
   return (
-    <div>Hello World</div>
+    <Container>
+      <div>Hello World</div>
+    </Container>
   );
 };
 

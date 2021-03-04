76bd6e7eba24654a2b69b087ba24790f38fd3271
idbd <idbd.young@gmail.com>
2021-03-04 18:33:43 +0900

    feat: Add link tag for the font-style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -25,5 +25,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link
+            href='https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap'
+            rel='stylesheet'
+          />
+          <link
+            href='https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }

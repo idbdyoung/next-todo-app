696dc6ae66b0461b5667c7e41bca342172e2a00b
idbd <idbd.young@gmail.com>
2021-03-05 19:57:11 +0900

    feat: Show todo-item on the list with mock data

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,13 +1,92 @@
 import styled from 'styled-components';
+import { TodoType } from '../types/todo';
+import palette from '../styles/palette';
 
 const Container = styled.div`
   width: 100%;
+  height: 100px;
+  .todo-list {
+    width: 100%;
+    height: 100%;
+    .todo-item {
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      height: 50px;
+      border-bottom: 1px solid ${palette.gray};
+      .todo-left-side {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        height: 100%;
+        .todo-color-block {
+          width: 12px;
+          height: 100%;
+        }
+        .todo-text {
+          display: flex;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+          margin-left: 12px;
+          font-size: 16px;
+        }
+        .checked-todo-text {
+          color: ${palette.gray};
+          text-decoration: line-through;
+        }
+      }
+      .todo-right-side {
+        width: 50px;
+        height: 100%;
+        background: ${palette.gray}
+      }
+    }
+  }
+  .bg-blue {
+    background-color: ${palette.blue};
+  }
+  .bg-green {
+    background-color: ${palette.green};
+  }
+  .bg-navy {
+    background-color: ${palette.navy};
+  }
+  .bg-orange {
+    background-color: ${palette.orange};
+  }
+  .bg-red {
+    background-color: ${palette.red};
+  }
+  .bg-yellow {
+    background-color: ${palette.yellow};
+  }
 `;
 
-const TodoList: React.FC = () => {
+interface IProps {
+  todos: TodoType[];
+}
+
+const TodoList: React.FC<IProps> = ({ todos }) => {
   return (
     <Container>
-      <h1>투두리스트 아이템이 올 자리</h1>
+      <ul className='todo-list'>
+        {
+          todos.map(todo => (
+            <li className='todo-item'>
+              <div className='todo-left-side'>
+                <div className={`todo-color-block bg-${todo.color}`}/>
+                <p className={`todo-text ${todo.checked? 'checked-todo-text' : ''}`}>
+                  {todo.text}
+                </p>
+              </div>
+              <div className='todo-right-side'>
+
+              </div>
+            </li>
+          ))
+        }
+      </ul>
     </Container>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import { NextPage } from 'next';
 import TodoList from '../components/TodoList';
+import { TodoType } from '../types/todo';
 
-const index: NextPage = () => {
-  return <TodoList />;
+interface IProps {
+  todos: TodoType;
+}
+
+const mockData: TodoType[] = [{'id':1,'text':'아무개','color':'red','checked':true},{'id':2,'text':'아무개나','color':'navy','checked':false}];
+
+const index: NextPage<IProps> = () => {
+  return <TodoList todos={mockData}/>;
 };
 
 export default index;

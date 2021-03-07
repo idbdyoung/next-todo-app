11cfb3d31977ef058a54e33fc754b8762d3f5c74
idbd <idbd.young@gmail.com>
2021-03-07 15:40:42 +0900

    feat: Load TodoList using a Data module

---
+++ pages/api/todos/index.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Data from '../../../lib/data';
+
+const todos = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    try {
+      const todos = await Data.todo.getTodoList();
+      res.statusCode = 200;
+
+      return res.send(todos);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  res.statusCode = 405;
+
+  return res.end();
+};
+
+export default todos;

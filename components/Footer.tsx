567c61d1e89577ea76c0b9de3448939ff41381b5
idbd <idbd.young@gmail.com>
2021-03-09 14:10:27 +0900

    feat: Add routing button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,46 @@
+import { useRouter } from 'next/dist/client/router';
 import styled from 'styled-components';
+import palette from '../styles/palette';
 
 const Container = styled.footer`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 500px;
+  width: 100%;
   height: 53px;
-  padding: 0 12px;
-  border-top: 1px solid gray;
+  position: fixed;
+  bottom: 0;
+  border-top: 1px solid ${palette.gray};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  .footer-button {
+    font-size: 32px;
+    width: 32px;
+    height: 32px;
+    border-radius: 5px;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    padding: 0;
+    line-height: 0;
+    outline: none;
+  }
 `;
 
 const Footer: React.FC = () => {
+  const router = useRouter();
+  const isMain = router.pathname === '/';
+
   return (
     <Container>
+      <button
+        type='button'
+        className='footer-button'
+        onClick={() => router.push(isMain ? '/todo/add' : '/')}
+      >
+        {isMain ? '+' : '-'}
+      </button>
     </Container>
   );
 };
